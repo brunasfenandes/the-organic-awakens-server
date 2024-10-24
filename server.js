e9a12570587279e7c5aa0ed6a51c82ed8575d35e
brunasfenandes <bruna.fenandes@outlook.com>
2024-10-24 18:42:10 -0400

    Serving public/images as static files test

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
   console.log('Middleware: Request received');
